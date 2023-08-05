e9392fa481fe832284ea22d93ea37130c603cf9e
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-08-05 10:00:36 +0000

    colorpicker

--- ColorPicker/js/script.js
+++ ColorPicker/js/script.js
@@ -1,1 +1,4 @@
-document.querySelector('#colorText').textContent = `カラーコード:${document.querySelector('#colorPicker').value}`;
+const text = document.querySelector('#colorText');
+const color = document.querySelector('#colorPicker');
+
+text.textContent = `カラーコード:${color.value}`;
