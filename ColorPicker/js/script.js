0e94f008e99b055be27559b6341f986eef9750d0
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-08-06 11:06:16 +0000

    colorpicker

--- ColorPicker/js/script.js
+++ ColorPicker/js/script.js
@@ -1,4 +1,9 @@
 const text = document.querySelector('#colorText');
 const color = document.querySelector('#colorPicker');
 
-text.textContent = `カラーコード:${color.value}`;
+const colorBg = () => {
+    document.body.style.backgroundColor = color.value;
+    text.textContent = `カラーコード:${color.value}`;
+}
+
+color.addEventListener('input', colorBg);
