7ce5a53cb78e2273661c9735abbd5756ea1d2737
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-08-09 05:13:09 +0000

    10-demo

--- 10-demo/js/script.js
+++ 10-demo/js/script.js
@@ -2,9 +2,5 @@ const isAgreed = document.querySelector('#check');
 const btn = document.querySelector('#btn');
 
 isAgreed.addEventListener('change', () => {
-  if (isAgreed.checked === true){
-    btn.disabled = false;
-  } else {
-    btn.disabled = true;
-  }
+  btn.disabled = !isAgreed.checked;
 })
