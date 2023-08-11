5a0d4b7ec2977719bf07b7b78abe65f494001a0c
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-08-11 12:05:37 +0000

    MenuList

---
+++ chapter5/MenuList/js/script.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector('#menu')
+
+const lists = [
+  'strawberry.jpg',
+  'lime.jpg',
+  'mango.jpg',
+  'lemon.jpg',
+  'fig.jpg',
+  'apple.jpg'
+];
+
+const content = `<div><img src="images/${lists[0]}" alt=""></div>
+  <div><img src="images/${lists[1]}" alt=""></div>
+  <div><img src="images/${lists[2]}" alt=""></div>
+  <div><img src="images/${lists[3]}" alt=""></div>
+  <div><img src="images/${lists[4]}" alt=""></div>
+  <div><img src="images/${lists[5]}" alt=""></div>
+`;
+
+menu.insertAdjacentHTML('beforeend', content);
