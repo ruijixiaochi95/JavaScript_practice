5097a62f9a415a73863a99aec358977d3a3d29d1
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-08-13 13:54:33 +0000

    09-demo

---
+++ chapter6/09-demo/js/script.js
@@ -0,0 +1,16 @@
+const items = document.querySelectorAll('.img-item');
+
+for (let i = 0; i < items.length; i++) {
+  const keyframes = {
+    opacity: [0, 1],
+    // rotate: ['x 90deg', 0],
+    translate: ['0 50px', 0],
+  };
+  const options = {
+    duration: 600,
+    delay: i * 300,
+    fill: 'forwards',
+  };
+
+  items[i].animate(keyframes, options);
+}
