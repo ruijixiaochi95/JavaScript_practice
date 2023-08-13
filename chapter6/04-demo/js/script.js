6cd2392980f27a2d866ed24638780dfdb4c2aa0e
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-08-13 12:19:26 +0000

    04-demo

--- chapter6/04-demo/js/script.js
+++ chapter6/04-demo/js/script.js
@@ -1,12 +1,22 @@
 const heading = document.querySelector('#heading');
 
 const keyframes = {
-  opacity: [0,1],
-  translate: ['0 50px', 0],
+  borderRadius: [
+    '20% 50% 50% 70%/50% 50% 70% 50%',
+    '50% 20% 50% 50%/40% 40% 60% 60%',
+    '50% 40% 20% 40%/40% 50% 50% 80%',
+    '50% 50% 50% 20%/40% 40% 60% 60%',
+    ],
+  // color: ['transparent', '#fff'],
+  // backgroundPosition: ['100% 0', '0 0']
+  // opacity: [0,1],
+  // rotate: ['x 360deg', 0],
 };
 const options = {
-  duration: 2000,
-  easing: 'ease',
+  duration: 8000,
+  // easing: 'ease',
+  direction: 'alternate',
+  iterations: Infinity,
 };
 
 heading.animate(keyframes, options);
