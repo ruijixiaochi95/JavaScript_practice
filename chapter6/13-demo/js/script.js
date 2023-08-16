740f70f9a5a331aaaef9276c1ca0d64d5a712763
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-08-16 12:39:33 +0000

    13-demo

--- chapter6/13-demo/js/script.js
+++ chapter6/13-demo/js/script.js
@@ -1,5 +1,10 @@
-const showKirin = () => {
-  console.log('キリンさんです');
+const showKirin = (entries) => {
+  const keyframes = {
+    opacity: [0, 1],
+    translate: ['200px 0', 0],
+  };
+  // console.log(entries[0].target);
+  entries[0].target.animate(keyframes, 600);
 };
 
 const kirinObserver = new IntersectionObserver(showKirin);
