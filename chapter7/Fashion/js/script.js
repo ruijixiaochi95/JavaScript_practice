1939c60d8c99b1a615ca2d571ad754d945f36cbe
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-08-17 09:27:59 +0000

    Fasion

---
+++ chapter7/Fashion/js/script.js
@@ -0,0 +1,18 @@
+
+
+const loadingAreaGrey = document.querySelector('#loading');
+
+window.addEventListener('load', () => {
+  loadingAreaGrey.animate(
+    {
+      opacity: [1, 0],
+      visibility: 'hidden',
+    },
+    {
+      duration: 2000,
+      delay: 1200,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+});
