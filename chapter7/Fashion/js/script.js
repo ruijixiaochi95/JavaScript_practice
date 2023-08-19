1b00ea8f68832bb7f0006a5f9eae993b166daa63
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-08-19 14:17:14 +0000

    Fasion

--- chapter7/Fashion/js/script.js
+++ chapter7/Fashion/js/script.js
@@ -51,10 +51,16 @@ window.addEventListener('load', () => {
 const mainImage = document.querySelector('.gallery-image img');
 const thumbImages = document.querySelectorAll('.gallery-thumbnails img');
 
-for(let i = 0; i < thumbImages.length; i++) {
-  thumbImages[i].addEventListener('mouseover', (event) => {
+// for(let i = 0; i < thumbImages.length; i++) {
+//   thumbImages[i].addEventListener('mouseover', (event) => {
+//     mainImage.src = event.target.src;
+//     mainImage.animate({opacity: [0, 1]}, 500);
+//   });
+// }
+thumbImages.forEach((thumbImage)=>{
+  thumbImage.addEventListener('mouseover', (event) => {
     mainImage.src = event.target.src;
     mainImage.animate({opacity: [0, 1]}, 500);
   });
-}
+});
 
