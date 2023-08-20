a8a799154f2faa14ac334b83ec02929f13c24204
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-08-20 09:20:35 +0000

    Fasion

--- chapter7/Fashion/js/script.js
+++ chapter7/Fashion/js/script.js
@@ -67,6 +67,7 @@ thumbImages.forEach((thumbImage)=>{
 const menuOpen = document.querySelector('#menu-open');
 const menuClose = document.querySelector('#menu-close');
 const menuPanel = document.querySelector('#menu-panel');
+const menuItems = document.querySelectorAll('#menu-panel li');
 const menuOptions = {
   duration: 1400,
   easing: 'ease',
@@ -76,8 +77,26 @@ const menuOptions = {
 menuOpen.addEventListener('click', () => {
   // console.log('メニューを開く')
   menuPanel.animate({translate: ['100vw', 0]}, menuOptions);
+  menuItems.forEach((menuItem, index) => {
+    // console.log(`${index}番目のリスト`);
+    menuItem.animate(
+      {
+        opacity: [0, 1],
+        translate: ['2rem', 0],
+      },
+      {
+        duration: 2400,
+        delay: 300 * index,
+        easing: 'ease',
+        fill: 'forwards',
+      }
+    );
+  });
 });
 
 menuClose.addEventListener('click', () => {
   menuPanel.animate({translate: [0, '100vw']}, menuOptions);
+  menuItems.forEach((menuItem) => {
+    menuItem.animate({opacity: [1, 0]}, menuOptions);
+  });
 });
