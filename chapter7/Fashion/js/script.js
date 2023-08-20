0445860f011cbc915fd62ec01fc85a6842e19abd
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-08-20 08:46:18 +0000

    Fasion

--- chapter7/Fashion/js/script.js
+++ chapter7/Fashion/js/script.js
@@ -64,3 +64,20 @@ thumbImages.forEach((thumbImage)=>{
   });
 });
 
+const menuOpen = document.querySelector('#menu-open');
+const menuClose = document.querySelector('#menu-close');
+const menuPanel = document.querySelector('#menu-panel');
+const menuOptions = {
+  duration: 1400,
+  easing: 'ease',
+  fill: 'forwards',
+};
+
+menuOpen.addEventListener('click', () => {
+  // console.log('メニューを開く')
+  menuPanel.animate({translate: ['100vw', 0]}, menuOptions);
+});
+
+menuClose.addEventListener('click', () => {
+  menuPanel.animate({translate: [0, '100vw']}, menuOptions);
+});
